7f7d99b67a3ce79c87d695b69296713df2abc4c1
Chyoba <cnoguoba@gmail.com>
2025-04-01 16:43:08 +0100

    feat; Data type, casting and introduction to jupyter notebook

---
+++ .ipynb_checkpoints/data_type-checkpoint.py
@@ -0,0 +1,42 @@
+"""
+xoa = str(243)
+xou = "Active"
+print(xoa + xou)
+
+"""
+xyz = "Hello world"
+print(xyz)
+print(type(xyz))
+
+xyz = 20
+print(xyz)
+print(type(xyz))
+
+xyz = 20.5
+print(xyz)
+print(type(xyz))
+
+xyz = 20j
+print(xyz)
+print(type(xyz))
+
+xyz = ["orange", "mango"]
+print(xyz)
+print(type(xyz))
+
+xyz = ("orange", "mango")
+print(xyz)
+print(type(xyz))
+
+xyz = (0, 6)
+print(xyz)
+print(type(xyz))
+
+xyz = {"orange", "mango"}
+print(xyz)
+print(type(xyz))
+
+xyz = {"fruit": "orange", "count": 10}
+print(xyz)
+print(type(xyz))
+
---
+++ data_type.py
@@ -0,0 +1,54 @@
+"""
+xoa = str(243)
+xou = "Active"
+print(xoa + xou)
+
+"""
+xyz = "Hello world"
+print(xyz)
+print(type(xyz))
+
+xyz = 20
+print(xyz)
+print(type(xyz))
+
+xyz = 20.5
+print(xyz)
+print(type(xyz))
+
+xyz = 20j
+print(xyz)
+print(type(xyz))
+
+xyz = ["orange", "mango"]
+print(xyz)
+print(type(xyz))
+
+xyz = ("orange", "mango")
+print(xyz)
+print(type(xyz))
+
+xyz = (0, 6)
+print(xyz)
+print(type(xyz))
+
+xyz = {"orange", "mango"}
+print(xyz)
+print(type(xyz))
+
+xyz = {"fruit": "orange", "count": 10}
+print(xyz)
+print(type(xyz))
+
+xyz = True
+print(xyz)
+print(type(xyz))                                                                                                                                                               
+
+xyz = False
+print(xyz)
+print(type(xyz))
+
+xyz = None
+print(xyz)
+print(type(xyz))
+
